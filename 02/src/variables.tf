aad90dd08e161ab5f5033cf2419aff2241c6cf04
world12hub <112606666+world12hub@users.noreply.github.com>
2026-06-22 15:52:46 +0300

    Update variables.tf

--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -3,11 +3,13 @@
 
 variable "cloud_id" {
   type        = string
+  default     = "b1gcehnmg326hj1svui2"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
 variable "folder_id" {
   type        = string
+  default     = "b1g133inh4mh0get24ds"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
