504cc76f417e191a279d077b2b5f334c08f596a3
world12hub <112606666+world12hub@users.noreply.github.com>
2026-06-23 09:18:19 +0300

    Update main.tf

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -10,8 +10,9 @@ resource "yandex_vpc_subnet" "develop" {
 
 
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
+  family = var.vm_web_image_family
 }
+
 resource "yandex_compute_instance" "platform" {
   name        = "netology-develop-platform-web"
   platform_id = "standard-v3"
