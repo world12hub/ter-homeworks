95b22acd88cdef8af86888e6dc88456d1aaf4067
Sergey Kanyugin <sergeykanyugin@gmail.com>
2026-09-12 15:22:42 +0300

    1

--- 03/src/for_each-vm.tf
+++ 03/src/for_each-vm.tf
@@ -21,7 +21,7 @@ resource "yandex_compute_instance" "platform-db" {
   }
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
+    nat       = false
   #  security_group_ids = [yandex_vpc_security_group.example.id]
   #  security_group_ids = count.index == 0 ? [yandex_vpc_security_group.example.id]: []
   }
