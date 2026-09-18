1f4e7b2bb550a9fb3ce15c6c59c6c04848b1746f
Sergey Kanyugin <sergeykanyugin@gmail.com>
2026-09-18 20:18:29 +0300

    add ssh public key

--- 04/demonstration1/vms/providers.tf
+++ 04/demonstration1/vms/providers.tf
@@ -12,5 +12,5 @@ provider "yandex" {
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   service_account_key_file = file("~/.authorized_key.json")
-  zone                     = var.default_zone
+  zone                     = var.zone-a
 }
--- 04/demonstration1/vms/variables.tf
+++ 04/demonstration1/vms/variables.tf
@@ -48,5 +48,5 @@ variable "cidr_develop_b" {
 
 variable "ssh_public_key" {
   type    = string
-  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGiVcfW8Wa/DxbBNzmQcwn7hJOj7ji9eoTpFakVnY/AI webinar"
+  description = "ssh public key"
 }
