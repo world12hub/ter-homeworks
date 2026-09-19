ed678e2f0448456b1133243375f4395ac4717fd8
Sergey Kanyugin <sergeykanyugin@gmail.com>
2026-09-19 17:52:03 +0300

    add vms with src

---
+++ 04/src/vms/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = "~>1.15.0"
+}
+
+provider "yandex" {
+  # token                    = "do not use!!!"
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  service_account_key_file = file("~/authorized_key.json")
+  zone                     = var.zone-a
+}
