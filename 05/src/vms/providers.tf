e9d0b51df708655e48eee497bd6b612dab2aca97
world12hub <sergeykanyugin@gmail.com>
2026-09-24 20:08:50 +0300

    add_s3

--- 05/src/vms/providers.tf
+++ 05/src/vms/providers.tf
@@ -1,10 +1,35 @@
 terraform {
-  required_providers {
+    required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
     }
   }
-  required_version = ">=1.15.0"  
+  
+  required_version = ">=1.15.0"
+
+  backend "s3" {
+    # Информация об использованном профиле и зоны доступности
+    # shared_credentials_files = ["~/.aws/credentials"]
+    profile                  = "default"
+    region                   = "ru-central1"
+    # Информация о бакете
+    bucket  = "tfstate-develop-world12hub" #  название бакета
+    key     = "dev1/dev1-terraform.tfstate"
+    encrypt = false
+
+    # НОВОЕ: Встроенный механизм блокировок (Terraform >= 1.6)
+    # Не требует отдельной базы данных (DynamoDB/YDB)!
+    use_lockfile = true
+    # Настройки yandex cloud
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+  }
 }
 
 provider "yandex" {
