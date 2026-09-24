59776dc913f23237b1643a691014be2b4115e488
world12hub <sergeykanyugin@gmail.com>
2026-09-24 21:18:24 +0300

    1

--- 05/src/vms/main.tf
+++ 05/src/vms/main.tf
@@ -23,23 +23,48 @@ module "vpc" {
 
   vpc_name   = "develop"
   zone           = "ru-central1-a"
-  v4_cidr_blocks = ["10.0.1.0/24"]
+  v4_cidr_blocks = var.cidr_develop_a
 }
 
 output "vpc_dev_subnet" {
   value = module.vpc.subnet
 }
 
+resource "yandex_vpc_security_group" "vm_sg" {
+  name       = "develop-vm-sg"
+  network_id = module.vpc.network.id
+
+  ingress {
+    protocol       = "TCP"
+    description    = "SSH"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 22
+  }
+
+  ingress {
+    protocol       = "ICMP"
+    description    = "Ping"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol       = "ANY"
+    description    = "Allow all outbound"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 module "test-vm" {
-  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=a230c79"
   env_name       = "develop" 
   network_id     = module.vpc.network.id
   subnet_zones   = [module.vpc.subnet.zone]
   subnet_ids     = [module.vpc.subnet.id] 
+  security_group_ids = [yandex_vpc_security_group.vm_sg.id]
   instance_name  = "webs"
   instance_count = 2
   image_family   = "ubuntu-2004-lts"
-  public_ip      = true
+  public_ip      = false
 
   labels = { 
     owner= "s.kanyugin",
@@ -54,15 +79,16 @@ module "test-vm" {
 }
 
 module "example-vm" {
-  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=a230c79"
   env_name       = "stage"
   network_id     = module.vpc.network.id
   subnet_zones   = [module.vpc.subnet.zone]
-  subnet_ids     = [module.vpc.subnet.id]  
+  subnet_ids     = [module.vpc.subnet.id] 
+  security_group_ids = [yandex_vpc_security_group.vm_sg.id]
   instance_name  = "web-stage"
   instance_count = 1
   image_family   = "ubuntu-2004-lts"
-  public_ip      = true
+  public_ip      = false
 
   labels = { 
       owner= "s.kanyugin",
